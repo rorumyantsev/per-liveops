70992d62e593a5514ace5d5ae20c994e9effba28
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-07-10 16:28:17 -0500

    Update wh_report_per.py

--- wh_report_per.py
+++ wh_report_per.py
@@ -215,7 +215,8 @@ def get_cached_report(option):
 
 
 routing_task = st.text_input("Enter routing task id")
-if routing_task is not None st.write(get_routing(routing_task))
+if routing_task is not None:
+    st.write(get_routing(routing_task))
 '''
 df = get_cached_report(option)        
 delivered_today = len(df[df['status'].isin(['delivered', 'delivered_finish'])])
