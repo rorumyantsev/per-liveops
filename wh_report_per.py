0c3fe4675351cf3b9532771f2df13570895ecc57
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-07-11 13:34:43 -0500

    Update wh_report_per.py

--- wh_report_per.py
+++ wh_report_per.py
@@ -16,7 +16,7 @@ CLIENT_LIST = st.secrets["CLIENTS"]
 #SHEET_ID = st.secrets["SHEET_ID"]
 API_URL = st.secrets["API_URL"]
 FILE_BUFFER = io.BytesIO()
-client_timezone = "America/Lime"
+client_timezone = "America/Lima"
 
 def check_for_lateness (row, wh_leaving_time):
     row["late"] = False
