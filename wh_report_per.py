48cb6cbc26226aaced661a50ef89ad2d7be835ad
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-07-11 15:07:31 -0500

    Update wh_report_per.py

--- wh_report_per.py
+++ wh_report_per.py
@@ -235,6 +235,7 @@ if len(routing_task) > 0:
         route_df = route_df.join(df.set_index("claim_id"),on = "claim",how = "left")
         wh_leaving_time = route_df[~route_df["point_A_time"].isin(["Point A was never visited"])]["point_A_time"].max()
         wh_leaving_time = datetime.datetime.strptime(wh_leaving_time, "%Y-%m-%d %H:%M:%S.%f%z")
+        st.write(type(wh_leaving_time))
         route_df = route_df.apply(lambda row: check_for_lateness(row, wh_leaving_time), axis = 1)
         expander = st.expander(f"Route id {route_df['route_id'][0]} | {route_df['courier_name'][0]}")
         expander.write(route_df)
