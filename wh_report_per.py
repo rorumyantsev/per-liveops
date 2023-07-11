6bfa0bda7b0359f2ab711228e2272133bd738a9e
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-07-11 05:21:32 -0500

    Update wh_report_per.py

--- wh_report_per.py
+++ wh_report_per.py
@@ -217,11 +217,10 @@ def get_cached_report(option):
 routing_task = st.text_input("Enter routing task id")
 if len(routing_task) > 0:
     routing = get_routing(routing_task)
-    
+    routes = []
     for route in routing["result"]["routes"]:
-        routes = []
+        result_route = []
         for route_point in route["route"]:
-            result_route = []
             #st.write(route_point)
             route_point_claim = route_point["node"]["value"]["id"]
             route_point_time_arrival = route_point["arrival_time_s"]
